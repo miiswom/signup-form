65bc07a168a83f4d9d87d9570a93425380aad46d
Farmata Kane <kane.farmata@gmail.com>
2023-09-07 10:42:02 +0100

    Test out a function to confirm pwd

--- script.js
+++ script.js
@@ -8,26 +8,30 @@ const patterns = {
     email: /^([a-z\d\.-]+)@([a-z\d.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
 };
 
-let pwd1 = document.getElementById('pwd').value;
-let pwd2 = document.getElementById('pwd_confirm').value; 
-let msg = document.getElementById('confirm_msg');
+// let pwd1test = document.getElementById('pwd').value;
+// let pwd2 = document.getElementById('pwd_confirm').value; 
+// let msg = document.getElementById('confirm_msg');
 
-// function validatePwd() {
-//     let pwd1 = document.getElementById('pwd').value;
-//     let pwd2 = document.getElementById('pwd_confirm').value; 
-//     let msg = document.getElementById('confirm_msg');
-//     msg = '';
-//     // console.log(pwd1, pwd2);
-//     if(pwd1 != 0) {
-//         if(pwd1 == pwd2) {
-//             console.log("It matches");
-//             msg.innerHTML = 'valid';
-//         } else {
-//             console.log("It doesn't matches");
-//             msg.innerHTML = 'invalid';
-//         }
-//     }
-// };
+
+
+function validatePwd() {
+    let pwd1test = document.getElementById('pwd');
+    let pwd2test = document.getElementById('pwd_confirm');
+    let msg = document.getElementById('confirm_msg');
+    // msg = '';
+    // console.log(pwd1, pwd2);
+    if(pwd1test.value != 0) {
+        if(pwd1test.value == pwd2test.value) {
+            console.log("It matches");
+            pwd2test.className = 'valid';
+            msg.textContent = 'valid';
+        } else {
+            console.log("It doesn't matches");
+            pwd2test.className = 'invalid';
+            msg.textContent = 'invalid';
+        }
+    }
+};
 
 function validate(field, regex) {
     if(regex.test(field.value)) {
