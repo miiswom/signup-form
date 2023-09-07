d98cb53e3330040d5de36f969f02983f9ad4321d
Farmata Kane <kane.farmata@gmail.com>
2023-09-07 08:57:43 +0100

    Perform regex check

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-// VALIDATION SCRIPT
-
-const inputs = document.querySelectorAll('input');
-
+const inputs = document.querySelectorAll('input.regextest');
 
 const patterns = {
-    phone_number: /^\d{11}$/
-}
+    phone_number: /^\d{11}$/,
+    first_name: /^[a-z]+$/i, 
+    last_name:/^[a-z]+$/i,
+    pwd:/^[\w@-_]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d.-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+};
 
-// VALIDATION FUNCTION
+let pwd1 = document.getElementById('pwd').value;
+let pwd2 = document.getElementById('pwd_confirm').value; 
+let msg = document.getElementById('confirm_msg');
 
+// function validatePwd() {
+//     let pwd1 = document.getElementById('pwd').value;
+//     let pwd2 = document.getElementById('pwd_confirm').value; 
+//     let msg = document.getElementById('confirm_msg');
+//     msg = '';
+//     // console.log(pwd1, pwd2);
+//     if(pwd1 != 0) {
+//         if(pwd1 == pwd2) {
+//             console.log("It matches");
+//             msg.innerHTML = 'valid';
+//         } else {
+//             console.log("It doesn't matches");
+//             msg.innerHTML = 'invalid';
+//         }
+//     }
+// };
 
 function validate(field, regex) {
-    console.log(regex.test(field.value));
-}
+    if(regex.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+};
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
-        // console.log(e.target.attributes.name.value)
         validate(e.target, patterns[e.target.attributes.name.value])
     });
 });
